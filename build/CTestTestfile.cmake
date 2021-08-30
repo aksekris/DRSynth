93be92af3f694197a8c444ece4b698d57d9b75ca
Trym Nordal <trym1994@gmail.com>
2021-08-30 12:45:59 +0200

    rm builddir

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/trymnordal/Documents/BasicProject
-# Build directory: /Users/trymnordal/Documents/BasicProject/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("third_party/googletest")
--- build/third_party/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/trymnordal/Documents/BasicProject/third_party/googletest/googletest
-# Build directory: /Users/trymnordal/Documents/BasicProject/build/third_party/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
